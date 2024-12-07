6ea3dc7272a355b65f6724abc11cc5f56c26db69
Trong Ngo <trong.ngotrung@gmail.com>
2024-12-07 18:02:07 +0700

    chore: add interceptors

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 //
 import { HttpTokenInterceptor } from './interceptors/http-token.interceptor';
 import { FooterComponent, HeaderComponent } from './layout';
-import { RouterModule } from '@angular/router';
 import { AppCommonModule } from '@app/common/common.module';
+import { ErrorInterceptor, TokenInterceptor } from './interceptors';
 
 const BASE_MODULES = [
     CommonModule,
@@ -35,7 +36,9 @@ const COMPONENTS = [
             provide: HTTP_INTERCEPTORS,
             useClass: HttpTokenInterceptor,
             multi: true
-        }
+        },
+        { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
+        { provide: HTTP_INTERCEPTORS, useClass: TokenInterceptor, multi: true },
     ]
 })
 export class CoreModule { }
